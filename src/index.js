20ed922bc82123d94754be4e6135a0cc46c0bbaa
Paul Patterson <pddpatterson@gmail.com>
2018-09-02 21:24:05 +0200

    change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import fastify from 'fastify';
 import helmet from 'fastify-helmet';
 import { notify, modem } from './plugins';
 import routes from './routes';
-import logger from './lib/logger';
 
 const server = fastify({ logger: true });
 
@@ -17,6 +16,4 @@ server.listen(3000, err => {
     server.log.error(err);
     process.exit(1);
   }
-
-  logger.log('info', 'Server running on port 3000');
 });
